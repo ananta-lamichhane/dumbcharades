83ad8471a138d39e419de961781e0f93a8052431
ananta-lamichhane <bloomsnruins@gmail.com>
2023-01-25 21:39:47 +0100

    cleanup, added comments to clarify the functionalities

--- src/App.js
+++ src/App.js
@@ -7,15 +7,24 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { ScoreCard } from "./components/Scores/Scorecard";
 import { TimerCard } from "./components/Timer/timer";
 import { useEffect,useState} from "react";
-import { Films } from "./data/films";
+
 
 export default function App() {
-  const [formSubmitted, setFormSubmitted] = useState(false)
-  const[formData, setFormData] = useState()
-  const[randomMovie, setRandomMovie] = useState("Movie here")
   
+  // keep track of if the form is submitted
+  const [formSubmitted, setFormSubmitted] = useState(false)
+
+  // save the data submitted in the form
+  const[formData, setFormData] = useState()
+  
+  // a callback function to retrieve data from child component by sending this
+  // function as a prop to the child (NewForm)
+
   function getFormDataFromChild(data){
     console.log(data)
+    // set the data from the child component (NewForm) and save it to the FormData
+    // set formSubmitted to true so the component can be hidden and the game interface
+    // can load
     setFormSubmitted(true)
     setFormData(data)
   }
@@ -23,9 +32,13 @@ export default function App() {
 
   return (
     <div className="App">
+      {/* wrap the components in ChakraProvider to be able to use Chakra UI theme palettes and such */}
       <ChakraProvider>
-        {!formSubmitted? <NewForm parentCallback={getFormDataFromChild}/>: 
+        {//if the form is not submitted yet then show the Form component
+        !formSubmitted? <NewForm parentCallback={getFormDataFromChild}/>: 
         <div>
+          {/*Otherwise show the score interface and pass the data collected
+          from the form as a prop */}
            <ScoreCard formData={formData}/>
            
           
