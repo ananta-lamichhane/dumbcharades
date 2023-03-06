c1f967958349c31eca66a1433c723f273afad181
ananta.lamichhane <bloomsnruins@gmail.com>
2023-03-06 22:38:15 +0100

    added routes for admin and regular gameplay

--- src/App.js
+++ src/App.js
@@ -8,9 +8,16 @@ import { ScoreCard } from "./components/Scores/Scorecard";
 import { TimerCard } from "./components/Timer/timer";
 import { useEffect,useState} from "react";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Routes
+} from "react-router-dom";
+
 
 export default function App() {
-  
   // keep track of if the form is submitted
   const [formSubmitted, setFormSubmitted] = useState(false)
 
@@ -20,8 +27,10 @@ export default function App() {
   // a callback function to retrieve data from child component by sending this
   // function as a prop to the child (NewForm)
 
+
   function getFormDataFromChild(data){
     console.log(data)
+    localStorage.setItem('gameConfig', JSON.stringify(data))
     // set the data from the child component (NewForm) and save it to the FormData
     // set formSubmitted to true so the component can be hidden and the game interface
     // can load
@@ -29,21 +38,53 @@ export default function App() {
     setFormData(data)
   }
 
-
+  useEffect(() => {
+    let gameConfigData = localStorage.getItem('gameConfig')
+    console.log("gameconfig data")
+    console.log(gameConfigData)
+  
+    if(gameConfigData){
+      setFormData(JSON.parse(gameConfigData))
+      setFormSubmitted(true)
+    }
+  },[]);
   return (
     <div className="App">
       {/* wrap the components in ChakraProvider to be able to use Chakra UI theme palettes and such */}
       <ChakraProvider>
-        {//if the form is not submitted yet then show the Form component
-        !formSubmitted? <NewForm parentCallback={getFormDataFromChild}/>: 
-        <div>
-          {/*Otherwise show the score interface and pass the data collected
-          from the form as a prop */}
-           <ScoreCard formData={formData}/>
-           
-          
-        </div>
-       }
+        <Router>
+          <Routes>
+          <Route path="/admin" element={
+            <div>
+              {//if the form is not submitted yet then show the Form component
+                !formSubmitted? <NewForm parentCallback={getFormDataFromChild}/>: 
+                <div>
+                  {/*Otherwise show the score interface and pass the data collected
+                  from the form as a prop */}
+                  <ScoreCard formData={formData}/>
+                  
+                  
+                </div>
+              }
+            </div>
+          } />
+          <Route path="/" element={
+            <div>
+              {//if the form is not submitted yet then show the Form component
+                !formSubmitted? <NewForm parentCallback={getFormDataFromChild}/>: 
+                <div>
+                  {/*Otherwise show the score interface and pass the data collected
+                  from the form as a prop */}
+                  <ScoreCard formData={formData}/>
+                  
+                  
+                </div>
+              }
+            </div>
+          } />
+          </Routes>
+        </Router>
+
         
       </ChakraProvider>
     </div>
