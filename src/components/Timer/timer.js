506c125b36c47a0259fc2d1a45347ee631b009b9
ananta.lamichhane <bloomsnruins@gmail.com>
2023-03-14 21:37:01 +0100

    bugfixes, responsive design

--- src/components/Timer/timer.js
+++ src/components/Timer/timer.js
@@ -1,8 +1,8 @@
-import { Card, CardHeader, Heading, CardFooter,
-        CardBody, Text, Button, SimpleGrid, HStack } from "@chakra-ui/react"
+import { Flex, Card, CardHeader, Heading, CardFooter,
+        Divider,VStack, CardBody, Text, Button, SimpleGrid, HStack } from "@chakra-ui/react"
 import { useEffect, useState } from "react"
 
-import { HiStar } from "react-icons/hi"
+import { HiStar, HiCheck, HiX, HiPlay, HiPause, HiStop  } from "react-icons/hi"
 import { englishMovies, Films, nepaliMovies } from "../../data/films"
 
 //pull this from form later
@@ -73,123 +73,175 @@ const TimerCard = (props) =>{
   
 
   return(
-    <div>
-    <SimpleGrid templateColumns='repeat(auto-fill, minmax(400px, 1fr))'>
-    <Card size={"sm"}>
-      <CardHeader>
-        <Heading fontSize={"6xl"} size='lg'> {
-          counter
-        } </Heading>
-      </CardHeader>
-      <CardBody>
-        <Text>Time is running out.</Text>
-      </CardBody>
-      
-    </Card>
-    {
-    <Card>
-      <CardHeader>
-        <Text fontSize={"2xl"}>Admin Controls</Text>
-      </CardHeader>
-      <CardBody>
-        <Text fontSize={"2xl"}>{gameState["nextMovie"]}</Text>
-     
-        <HStack>
-        <Button onClick={()=>{
-         toggleTimer()
-          }}>{pause?"Start":"Pause"}</Button>
-        <Button onClick={()=>{
-          resetCounter(GAMETIME)
+    <VStack
+    width={{base: "100%", md: "33%"}} >
+      <Card  
+        padding={"5px"} 
+        minWidth={"100px"}
+        width="100%"
+        >
+        <CardHeader>
+          <Heading fontSize={"6xl"} size='lg'> {
+            counter
+          } </Heading>
+        </CardHeader>
+        <CardBody>
+          <Button margin={"5px"} variant={"outline"} onClick={()=>{
+                toggleTimer()
+                  }}>{pause?<HiPlay size={"40px"} />:<HiPause size={"40px"} />}
+          </Button>
+          <Button margin={"5px"} onClick={
+                ()=>{
+                  passed?resetCounter(PASSTIME):resetCounter(GAMETIME)
+                  
+                  stopTimer()
+                  }
+                }>
+                {<HiStop size={"40px"} />}
+              </Button>
+        
+        </CardBody>
+        
+      </Card>
+      {
+      <Card padding={"5px"} minWidth={"100px"} width="100%">
+        <CardHeader>
+          <Text fontSize={"xl"}>Next Movie</Text>
+        </CardHeader>
+        <CardBody>
+            <Text fontSize={"4xl"}>{gameState["nextMovie"]}</Text>
+            <Divider></Divider>
+            <VStack marginTop={"10px"}>
+              <HStack>
+              <Button leftIcon={<HiCheck></HiCheck>} colorScheme={"green"} onClick={
+                ()=>{
+                  let newScore = 0
+                  let addScore = 0
+                  if(passed){
+                    addScore = parseInt(props.gameData["passedPts"])
+                    setPassed(false)
+                  }else{
+                    addScore= parseInt(props.gameData["correctPts"])
+                  }
+                  if(gameState["currentTeam"] === "team1"){
+                    newScore = (gameState["team1Score"] + addScore)
+                    let nextMovie = getRandomMovie()
+                    setGameState(gameState =>(
+                        {
+                        ...gameState,
+                        ["team1Score"]: newScore,
+                        ["currentTeam"]: "team2",
+                        ["nextMovie"]: nextMovie
+                      }
+                    ))
+                  }else{
+                    newScore = gameState["team2Score"] + addScore
+                    setGameState(gameState =>(
+                      {
+                        ...gameState,
+                        ["team2Score"]: newScore,
+                        ["currentTeam"]: "team1",
+                        ["nextMovie"]: getRandomMovie()
+                      }
+                      )
+                    )
+                  }
+                
+                  resetCounter(GAMETIME)
+                  stopTimer()
 
-          }}>Reset</Button>
-        <Button onClick={()=>{
-         
-          let newScore = 0
-          let addScore = 0
-          if(passed){
-            addScore = parseInt(props.gameData["passedPts"])
-            setPassed(false)
-          }else{
-            addScore= parseInt(props.gameData["correctPts"])
-          }
-          if(gameState["currentTeam"] === "team1"){
-            newScore = (gameState["team1Score"] + addScore)
-            let nextMovie = getRandomMovie()
-            setGameState(gameState =>({
-              ...gameState,
-              ["team1Score"]: newScore,
-              ["currentTeam"]: "team2",
-              ["nextMovie"]: nextMovie
-            }
-            ))
-          }else{
-
-            newScore = gameState["team2Score"] + addScore
-            setGameState(gameState =>({
-              ...gameState,
-              ["team2Score"]: newScore,
-              ["currentTeam"]: "team1",
-              ["nextMovie"]: getRandomMovie()
-            }
-            ))
-          }
+                }}>
+                Correct
+              </Button>
+            
+            <Button leftIcon={<HiX></HiX>} colorScheme={"red"} onClick={ () => 
+              {
+                if(!passed){
+                  resetCounter(PASSTIME)
+                  stopTimer()
+                  setPassed(true)
+                  
+                  
+                  if(gameState["currentTeam"] === "team1"){
+                  
+                    setGameState(gameState =>({
+                      ...gameState,
+                      ["currentTeam"]: "team2"
+                    }
+                    ))
+                  }else{
+                    setGameState(gameState =>({
+                      ...gameState,
+                      ["currentTeam"]: "team1"
+                    }
+                    ))
+                  }  
+                }else{
+                  setPassed(false)
+                  resetCounter(GAMETIME)
+                  if(gameState["currentTeam"] === "team1"){
           
-          resetCounter(GAMETIME)
-          stopTimer()
+                    setGameState(gameState =>({
+                      ...gameState,
+                      ["currentTeam"]: "team2",
+                      ["nextMovie"]: getRandomMovie()
+                    }
+                    ))
+                  }else{
+                    setGameState(gameState =>({
+                      ...gameState,
+                      ["currentTeam"]: "team1",
+                      ["nextMovie"]: getRandomMovie()
+                    }
+                    ))
+                  } 
 
-        }}>Correct Answer</Button>
-        <Button onClick={()=>{
-          resetCounter(PASSTIME)
-          stopTimer()
-          setPassed(true)
-          
-          
-          if(gameState["currentTeam"] === "team1"){
-           
-            setGameState(gameState =>({
-              ...gameState,
-              ["currentTeam"]: "team2"
-            }
-            ))
-            setPassed(true)
-          }else{
-            setGameState(gameState =>({
-              ...gameState,
-              ["currentTeam"]: "team1"
-            }
-            ))
-            setPassed(true)
-          }  
+                }
 
-        }}>Pass</Button>
-        <Button onClick={
-          ()=>{
-            if(gameState["currentTeam"] === "team1"){
-           
-              setGameState(gameState =>({
-                ...gameState,
-                ["currentTeam"]: "team2"
+
               }
-              ))
-             
-            }else{
-              setGameState(gameState =>({
-                ...gameState,
-                ["currentTeam"]: "team1"
+
+              }>
+              {
+                passed?"Wrong": "Pass"
               }
-              ))
-             
-            } 
-          }
-        }>Toggle Team</Button>
-        </HStack>
+            
+            </Button>
+            </HStack>
+            <HStack>
+
+            <Button onClick={
+                ()=>{
+                  if(gameState["currentTeam"] === "team1"){
+                
+                    setGameState(gameState =>({
+                      ...gameState,
+                      ["currentTeam"]: "team2"
+                    }
+                    ))
+                  
+                  }else{
+                    setGameState(gameState =>({
+                      ...gameState,
+                      ["currentTeam"]: "team1"
+                    }
+                    ))
+                  
+                  } 
+                }
+              }>
+              Toggle Team
+            </Button>
+          </HStack>
+
+            </VStack>
 
 
-      </CardBody>
-    </Card>
-    }
-    </SimpleGrid>
-    </div>
+
+        </CardBody>
+      </Card>
+      }
+    </VStack>
 )
 }
 
